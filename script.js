2eec238fc84f95862edfc98fbbd3abfa3aa98708
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-12 18:38:24 +0600

    rebooted all work finish

--- script.js
+++ script.js
@@ -6,25 +6,28 @@ const userReview = document.getElementById('userReview');
 let sum = 1;
 
 submitBtn.addEventListener('click', function() {
-    const values = inputValue.value;
+    const values = inputValue.value.trim();
+   if( values.length > 0) {
     const para = document.createElement('p');
     para.innerText = sum + '. ' + values;
     para.classList.add('paragraph');
     userReview.appendChild(para);
     inputValue.value = '';
     sum++
+   }
 })
 
 
 inputValue.addEventListener('keyup', function(event) {
-    if( event.key === 'Enter') {
-        const values = inputValue.value;
-        const para = document.createElement('p');
-        para.innerText = sum + '. ' + values;
-        para.classList.add('paragraph');
-        userReview.appendChild(para);
-        inputValue.value = '';
-        sum++
+
+            const values = inputValue.value.trim();
+    if( event.key === 'Enter' && values.length > 0) {
+            const para = document.createElement('p');
+            para.innerText = sum + '. ' + values;
+            para.classList.add('paragraph');
+            userReview.appendChild(para);
+            inputValue.value = '';
+            sum++
     }
 })
 
